0250c02c93b57b898d9b0f9ce7036d2d9b6c96aa
rresch2004 <59953979+rresch2004@users.noreply.github.com>
2023-03-23 11:58:40 +0100

    Logger update

--- src/Test.php
+++ src/Test.php
@@ -19,6 +19,35 @@ class Test extends TestCase
         // assert
         $this->assertEquals($result, 2);
     }
+
+    /**
+     * @param $z1
+     * @param $z2
+     * @param $erg
+     * @return void
+     * @dataProvider numberProvider
+     */
+    public function testRandomNum($z1, $z2, $erg)
+    {
+        // arrange
+        $logger = new EchoLogger();
+        $calc = new Calc($logger);
+
+        // act
+        $result = $calc->add($z1, $z2);
+
+        // assert
+        $this->assertEquals($result, $erg);
+    }
+
+    public static function numberProvider(): array
+    {
+        return [
+            [1, 2, 3],
+            [4,4,8],
+            [5,5,10]
+    ];
+    }
 }
 
 
